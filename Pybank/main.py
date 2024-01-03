fb0fc98b1cdf428c541f807eb079cce526abd7dd
MissDysart <kelsy.dysart@gmail.com>
2024-01-02 22:39:24 -0700

    1. Add the CSV 'Resource' for the Pybank portion of the Python (module 3) Challenge. 2. Test some changes to PyBank "main.py"

---
+++ Pybank/main.py
@@ -0,0 +1,22 @@
+#Import the Modules we need
+import os
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+#Read the csv file for bank
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
